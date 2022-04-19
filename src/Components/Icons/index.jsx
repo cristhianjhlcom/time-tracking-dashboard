10bc1cde36327502caca705c8a5436ffed733348
cristhianjhlweb <cristhianjhl.web@gmail.com>
2022-04-18 20:23:05 -0500

    creación de componente Icons

---
+++ src/Components/Icons/index.jsx
@@ -0,0 +1,14 @@
+import { Code } from './Code';
+import { Dot } from './Dot';
+
+export { Code } from './Code';
+export { Dot } from './Dot';
+
+const Icon = {};
+
+Object.assign(Icon, {
+	Dot,
+	Code
+});
+
+export default Icon;
